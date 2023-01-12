ca06d98245d814d1421f87c9c3a7e4a52edd209c
George Stuyt <stugeor@hotmail.com>
2023-01-13 10:56:45 +1100

    Add dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name='pybpoddata',
     author='George Stuyt',
     packages=['pybpoddata'],
-    description="Interact with SessionData .mat files from a MATLAB Bpod installation as a dict or an object"
+    description="Interact with SessionData .mat files from a MATLAB Bpod installation as a dict or an object",
+    install_requires=['numpy',
+                      'matplotlib',
+                      'scipy',
+                      'seaborn',
+                      'pandas']
 )
