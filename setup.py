ab440ef8137bc1e490e2e8c4c47579d71efb954c
George Stuyt <stugeor@hotmail.com>
2023-01-13 11:00:57 +1100

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybpoddata',
+    version='1.0.0',
     author='George Stuyt',
     packages=['pybpoddata'],
     description="Interact with SessionData .mat files from a MATLAB Bpod installation as a dict or an object",
