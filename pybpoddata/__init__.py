269e57e3f592880647879f42122d5eea4d931436
George Stuyt <stugeor@hotmail.com>
2023-06-10 15:36:48 +1000

    Add submodules into package namespace

--- pybpoddata/__init__.py
+++ pybpoddata/__init__.py
@@ -1,2 +1,7 @@
 from .classes import SessionDataClass, AbstractTrialClass
 from .io import load_sessiondata_dict
+
+from . import analysis
+from . import classes
+from . import plot
+from . import io
